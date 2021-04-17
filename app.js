9e31f8c04a819977e3368634ba2838d788cebe51
alexdm1998 <62345984+alexdm1998@users.noreply.github.com>
2021-04-17 21:20:15 +0100

    Process Env added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 var app = express();
 app.use(express.json())
+let port = process.env.PORT || 3000;
 
-app.listen(3000);
+
 app.use(express.static(__dirname))
 module.exports = app;
 
 require('./controller/routes.js')
+
+app.get("/", (req,res)=>{
+    res.send("This is the root page of the App")
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}` )
+})

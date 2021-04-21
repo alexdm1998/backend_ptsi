fbbb37dec2b7247940d7091f4b847b13f75badff
alexdm1998 <62345984+alexdm1998@users.noreply.github.com>
2021-04-21 13:36:24 +0100

    Routes V2

--- controller/controllerUNICEF.js
+++ controller/controllerUNICEF.js
@@ -65,6 +65,75 @@ function getDataflowFiltered(req,res){
 }
 
 
+function getDataStructureV2(req, res){
+  var request = require('request');
+  var options = {
+    'method': 'GET',
+    'url': 'https://sdmx.data.unicef.org/ws/public/sdmxapi/rest/dataflow/all/all/latest/?format=sdmx-json&detail=full&references=none',
+    'header': 'Content-Type: application/json',
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    var ArrayDfID = [];
+    var ParsedJson = JSON.parse(response.body);
+    var ArrayDataflows = ParsedJson.data.dataflows;
+  
+    for(i = 0; i < ArrayDataflows.length; i++){
+    var DfID = ArrayDataflows[i].id;   
+    var DFName = ArrayDataflows[i].name;
+    var DFAgency = ArrayDataflows[i].agencyID;
+    var DfObject = [DfID,DFName,DFAgency];
+    ArrayDfID.push(DfObject);
+    }
+    
+    //var myJsonString = JSON.stringify(ArrayDfID);
+    res.status(200).json(ArrayDfID);
+
+    res.end();
+  });
+}
+
+function getDataflowV2(req,res){
+  var DataflowId = req.params.DfID;
+  var DataflowAgency = req.params.DfAgency;
+  var request = require('request');
+  var options = {
+    'method': 'GET',
+    'url': `https://sdmx.data.unicef.org/ws/public/sdmxapi/rest/data/${DataflowAgency},${DataflowId},1.0/all?format=sdmx-json`,
+    'header': 'Content-Type: application/json',
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    var JsonObj = JSON.parse(response.body)
+    res.status(200).json(JsonObj);
+    res.end();
+  });
+}
+
+
+function getDataflowFilteredV2(req,res){
+  var request = require('request');
+  var DataflowId = req.params.DfID;
+  var DataflowAgency = req.params.DfAgency;
+  var DataflowFilter = req.params.DfFilter;
+  var options = {
+    'method': 'GET',
+    'url': `https://sdmx.data.unicef.org/ws/public/sdmxapi/rest/data/${DataflowAgency},${DataflowId},1.0/${DataflowFilter}?format=sdmx-json`,
+  };
+  request(options, function(error, response){
+    if (error) throw new Error(error);
+    var JsonObj = JSON.parse(response.body);
+    res.status(200).json(JsonObj);
+  });
+
+}
+
+
+
+
+
+
+
 function getDataflowPage(req,res){
   res.sendFile(path.resolve('views/dataflows.html'));
 }
@@ -72,7 +141,10 @@ function getDataflowPage(req,res){
 
 module.exports = {
     getDataStructure: getDataStructure,
+    getDataStructureV2: getDataStructureV2,
     getDataflow: getDataflow,
+    getDataflowV2: getDataflowV2,
     getDataflowFiltered: getDataflowFiltered,
+    getDataflowFilteredV2: getDataflowFilteredV2,
     getDataflowPage: getDataflowPage
 };
--- controller/routes.js
+++ controller/routes.js
@@ -8,10 +8,16 @@ app.get('/INEjson/', controllerINE.getJson);
 
 
 const controllerUNICEF = require('./controllerUNICEF');
-app.get('/UNICEF/Datastructure', controllerUNICEF.getDataStructure);
-app.get('/UNICEF/Dataflow/:DfID/:DfAgency/request', controllerUNICEF.getDataflow);
-app.get('/UNICEF/Dataflow/:DfID/:DfAgency/filter/:DfFilter', controllerUNICEF.getDataflowFiltered);
-app.get('/UNICEF/Dataflow/:DfID/:DfAgency', controllerUNICEF.getDataflowPage); //Html page
+app.get('/UNICEF/Datastructure', controllerUNICEF.getDataStructure); //Não usem este
+app.get('/UNICEF/Dataflow/:DfID/:DfAgency/request', controllerUNICEF.getDataflow);//Não usem este
+app.get('/UNICEF/Dataflow/:DfID/:DfAgency/filter/:DfFilter', controllerUNICEF.getDataflowFiltered);//Não usem este
+
+
+
+app.get('/UNICEF/v2/Datastructure/', controllerUNICEF.getDataStructureV2); //Retorna datastructure
+app.get('/UNICEF/v2/Dataflow/:DfID/:DfAgency/request', controllerUNICEF.getDataflowV2); //Retorna dataflow sem filtro
+app.get('/UNICEF/v2/Dataflow/:DfID/:DfAgency/filter/:DfFilter', controllerUNICEF.getDataflowFilteredV2); //Retorna dataflow com filtro
+app.get('/UNICEF/Dataflow/:DfID/:DfAgency', controllerUNICEF.getDataflowPage); //Retorna um pag Html
 
 
 app.get('/', function (req, res) {

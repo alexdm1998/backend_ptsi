9164f405289cbf32278e37c430dc473c4b0ab662
alexdm1998 <62345984+alexdm1998@users.noreply.github.com>
2021-04-25 19:04:10 +0100

    Use Cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 var app = express();
 app.use(express.json())
 let port = process.env.PORT || 3000;
 let host = process.env.HOST || '127.0.0.1';
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+app.use(cors());
 module.exports = app;
 
 require('./controller/routes.js')
